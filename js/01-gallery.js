6d1b1a1c263388817cf5415bf0a999caf74922e4
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-07 23:42:21 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,17 +7,62 @@ const gallery = document.querySelector(".gallery");
 console.log(gallery);
 
 galleryItems.forEach((elem) => {
-  const newElem = `<li><img src="${elem.preview}" alt="${elem.description}"></img></li>`;
+  const newElem = `<div class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      data-source="${elem.original}"
+      alt="${elem.description}"
+    />
+  </a>
+</div>`;
   gallery.insertAdjacentHTML("beforeend", newElem);
 });
 
-gallery.addEventListener("click", (elem) => {
+gallery.addEventListener("click", openModalWithNewPictures);
+//
+
+function openModalWithNewPictures(elem) {
+  elem.preventDefault();
+  window.addEventListener("keydown", closeModalWithNewPicturesEsc);
   const elemTarget = elem.target;
-  galleryItems.forEach((elem) => {
-    if (elemTarget.src === elem.preview) {
-      const instance = basicLightbox.create(`
-    <img src="${elem.original}">`);
-      instance.show();
-    }
-  });
-});
+  const instance = basicLightbox.create(`
+    <img src="${elemTarget.dataset.source}">`);
+  instance.show();
+}
+
+function closeModalWithNewPicturesEsc(event) {
+  console.log(event.code);
+  if (event.code === "Escape") {
+    gallery.removeEventListener("click", openModalWithNewPictures);
+  }
+}
+
+//
+
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+
+// console.log(galleryItems);
+
+// const gallery = document.querySelector(".gallery");
+// console.log(gallery);
+
+// galleryItems.forEach((elem) => {
+//   const newElem = `<li><img src="${elem.preview}" alt="${elem.description}"></img></li>`;
+//   gallery.insertAdjacentHTML("beforeend", newElem);
+// });
+
+// gallery.addEventListener("click", (elem) => {
+//   const elemTarget = elem.target;
+
+//   galleryItems.forEach((elem) => {
+//     if (elemTarget.src === elem.preview) {
+//       const instance = basicLightbox.create(`
+//     <img src="${elem.original}">`);
+//       instance.show();
+//     }
+//   });
+
+// });
