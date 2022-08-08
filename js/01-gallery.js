4fb09f3d4d9223385dd8172d1d60df3f323187f5
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-08 12:32:48 +0300

    Copmlete task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,44 +25,25 @@ gallery.addEventListener("click", openModalWithNewPictures);
 
 function openModalWithNewPictures(elem) {
   elem.preventDefault();
+  //
   window.addEventListener("keydown", closeModalWithNewPicturesEsc);
+  //
   const elemTarget = elem.target;
+  //
   const instance = basicLightbox.create(`
     <img src="${elemTarget.dataset.source}">`);
+  //
   instance.show();
-}
-
-function closeModalWithNewPicturesEsc(event) {
-  console.log(event.code);
-  if (event.code === "Escape") {
-    gallery.removeEventListener("click", openModalWithNewPictures);
+  //
+  const backDropModal = document.querySelector(".basicLightbox");
+  backDropModal.addEventListener("click", () => {
+    window.removeEventListener("keydown", closeModalWithNewPicturesEsc);
+  });
+  //
+  function closeModalWithNewPicturesEsc(event) {
+    console.log(event.code);
+    if (event.code === "Escape") {
+      backDropModal.remove();
+    }
   }
 }
-
-//
-
-// import { galleryItems } from "./gallery-items.js";
-// // Change code below this line
-
-// console.log(galleryItems);
-
-// const gallery = document.querySelector(".gallery");
-// console.log(gallery);
-
-// galleryItems.forEach((elem) => {
-//   const newElem = `<li><img src="${elem.preview}" alt="${elem.description}"></img></li>`;
-//   gallery.insertAdjacentHTML("beforeend", newElem);
-// });
-
-// gallery.addEventListener("click", (elem) => {
-//   const elemTarget = elem.target;
-
-//   galleryItems.forEach((elem) => {
-//     if (elemTarget.src === elem.preview) {
-//       const instance = basicLightbox.create(`
-//     <img src="${elem.original}">`);
-//       instance.show();
-//     }
-//   });
-
-// });
