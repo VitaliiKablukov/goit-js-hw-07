3df933df621a8aaaff85faabe0b5e3bd21bec828
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-08-08 16:09:15 +0300

    complete task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+galleryItems.forEach((elem) => {
+  const newElem = `<a class="gallery__item" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+</a>`;
+  gallery.insertAdjacentHTML("beforeend", newElem);
+});
+const lightBoxGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.addEventListener("click", openModalWithNewPictures);
+//
+function openModalWithNewPictures(elem) {
+  elem.preventDefault();
+  lightBoxGallery.on("show.simplelightbox", function () {});
+}
